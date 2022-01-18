35cdb4860620b90d0ccf94a28134ede3e3fbb350
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-18 17:24:37 +0300

    Update create_ads.py

--- create_ads.py
+++ create_ads.py
@@ -2,17 +2,17 @@ from facebook_business.adobjects.adaccount import AdAccount
 from facebook_business.adobjects.ad import Ad
 from facebook_business.api import FacebookAdsApi
 
-access_token = 'EAAGbvZAOZBb6ABANr2iam1UYZA6LZAFUKDRVzvnt1G11Nc3VZAigrIXQfdvcKsFiv7SSYwwMuTH6bUFKpbfXa0BqsX3W0ogClZAJpqWNIDtHRx2jZB6vU7iIdIqcJkfAadet0lm8YVLaqMLwLNHVXhnq48jnbmcDMaRSZBj7ZBzNiTLTWFHuPZCLtZCLKAeqDwqAAApvRDtEqiGM5BLIeBnZCOZAH'
-app_secret = 'd71d19dd24ec5e70abc92bbf9fd20856'
-app_id = '452713506566048'
-id = 'act_4997533496957271'
+access_token = 'acces-token'
+app_secret = 'app-secret'
+app_id = 'app-id'
+id = 'ad-account-id'
 FacebookAdsApi.init(access_token=access_token)
 
 fields = [
 ]
 params = {
   'name': 'My Python Ad',
-  'adset_id': '23849368774600502',
+  'adset_id': 'ad-set-id',
   'creative': {'creative_id': '23849368927450502'},
   'status': 'PAUSED',
 }

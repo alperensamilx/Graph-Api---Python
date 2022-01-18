95aced33ab2b378098c4cba39c7e0aaf9ab5d51c
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-18 17:23:38 +0300

    Update call_adsets.py

--- call_adsets.py
+++ call_adsets.py
@@ -2,10 +2,10 @@ from facebook_business.adobjects.campaign import Campaign
 from facebook_business.adobjects.adset import AdSet
 from facebook_business.api import FacebookAdsApi
 
-access_token = 'EAAGbvZAOZBb6ABANr2iam1UYZA6LZAFUKDRVzvnt1G11Nc3VZAigrIXQfdvcKsFiv7SSYwwMuTH6bUFKpbfXa0BqsX3W0ogClZAJpqWNIDtHRx2jZB6vU7iIdIqcJkfAadet0lm8YVLaqMLwLNHVXhnq48jnbmcDMaRSZBj7ZBzNiTLTWFHuPZCLtZCLKAeqDwqAAApvRDtEqiGM5BLIeBnZCOZAH'
-app_secret = 'd71d19dd24ec5e70abc92bbf9fd20856'
-app_id = '452713506566048'
-id = 'act_4997533496957271'
+access_token = 'acces-token'
+app_secret = 'app-secret'
+app_id = 'app-id'
+id = 'ad-account-id'
 FacebookAdsApi.init(access_token=access_token)
 
 fields = [

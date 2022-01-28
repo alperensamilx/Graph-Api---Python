0c47f50a81a3c05a7522c41052555986610941dc
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-01-28 16:50:40 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,6 +4,8 @@ from facebook_business.adobjects.campaign import Campaign
 from facebook_business.adobjects.adset import AdSet
 from facebook_business.adobjects.ad import Ad
 from facebook_business.adobjects.adcreative import AdCreative
+from facebook_business.adobjects.page import Page
+from facebook_business.adobjects.pagepost import PagePost
 
 from facebook_business.api import FacebookAdsApi
 
@@ -14,10 +16,32 @@ id = '{AD_ACCPUNT_ID}'
 FacebookAdsApi.init(access_token=access_token)
 
 
+from facebook_business.adobjects.adaccount import AdAccount
+
+from facebook_business.adobjects.campaign import Campaign
+from facebook_business.adobjects.adset import AdSet
+from facebook_business.adobjects.ad import Ad
+from facebook_business.adobjects.adcreative import AdCreative
+from facebook_business.adobjects.page import Page
+from facebook_business.adobjects.pagepost import PagePost
+
+from facebook_business.api import FacebookAdsApi
+
+access_token = 'EAAQmOO1AkaoBAPbES3ZCGvwMk6dcnOCCetbAB9e3Fwxm9balFkXZAUKP8mKAMMx4qU3OR1zJvjuGX6iOb30Au778xNcOsZAqWIF1YByf44GoC7uIi1KOBUGLDUMXqKjoZAjyV41v41fHNlrLJ94nu5X8bcNx1W836XtZAFpvccyoZBaFN78jdlTa3X0XqbtY8vmQAllAk9AgZDZD'
+app_secret = '3c074e1ee7a8239f64bf469eed224d05'
+app_id = '1167925847298474'
+id = 'act_68601302'
+FacebookAdsApi.init(access_token=access_token)
+
+
 def create_campaigns():
     name = str(input('Campaign Name: '))
-    special_ad_categories = list(input('Special Ad Categories: '))
+    print('NONE, EMPLOYMENT, HOUSING, CREDIT, ISSUES_ELECTIONS_POLITICS, ONLINE_GAMBLING_AND_GAMING')
+    special_ad_categories = str(input('Special Ad Categories: '))
+    print(
+        'Use one of: APP_INSTALLS, BRAND_AWARENESS, EVENT_RESPONSES, LEAD_GENERATION, LINK_CLICKS, LOCAL_AWARENESS, MESSAGES, OFFER_CLAIMS, PAGE_LIKES, POST_ENGAGEMENT, PRODUCT_CATALOG_SALES, REACH, STORE_VISITS, VIDEO_VIEWS, CONVERSIONS.')
     objective = str(input('Campaign objective: '))
+    print('PAUSED')
     status = str(input('Status: '))
 
     fields = [
@@ -30,7 +54,7 @@ def create_campaigns():
         'special_ad_categories': special_ad_categories,
     }
 
-    data = (AdAccount(id).create_campaign(fields=fields, params=params))
+    data = AdAccount(id).create_campaign(fields=fields, params=params)
     print(data)
 
 
@@ -77,7 +101,7 @@ def create_ad_sets():
         'status': status,
     }
 
-    data = (AdAccount(id).create_ad_set(fields=fields, params=params, ))
+    data = AdAccount(id).create_ad_set(fields=fields, params=params)
     print(data)
 
 
@@ -108,7 +132,7 @@ def ad_creative():
                                   "description": description,
                               }},
     }
-    data = (AdAccount(id).create_ad_creative(fields=fields, params=params, ))
+    data = AdAccount(id).create_ad_creative(fields=fields, params=params)
     print(data)
 
 
@@ -122,7 +146,9 @@ def create_ads():
     status = str(input('Status: '))
 
     fields = [
-        'preview_shareable_link'
+        'name',
+        'preview_shareable_link',
+
     ]
     params = {
         'name': name,
@@ -131,7 +157,7 @@ def create_ads():
         'status': status,
     }
 
-    data = (AdAccount(id).create_ad(fields=fields, params=params,))
+    data = AdAccount(id).create_ad(fields=fields, params=params)
     print(data)
 
 
@@ -146,11 +172,11 @@ def call_campaigns():
     params = {
       'effective_status': ['PAUSED'],
     }
-    data = AdAccount(id).get_campaigns(fields=fields, params=params,)
+    data = AdAccount(id).get_campaigns(fields=fields, params=params)
     print(data)
 
 
-# call_campaigns()
+call_campaigns()
 
 
 def call_ad_sets():
@@ -169,103 +195,68 @@ def call_ad_sets():
     print(data)
 
 
+# call_ad_sets()
 
 
+def create_video():
+    fields = [
+    ]
+    params = {
+        'name': 'Video Creative',
+
+        'object_story_spec': {'page_id': '103122378948499',
+                              'video_data': {
+                                          'image_url': 'https://scontent.fist4-1.fna.fbcdn.net/v/t1.18169-9/11701119_1096804877014160_4246085576298344409_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=RU_deGQrNEkAX9KwO2B&_nc_ht=scontent.fist4-1.fna&oh=00_AT-NqO6aiivQOOQ6eyzi9ShJmQqLh51BAUc-ABfyYPo5yQ&oe=62150B12',
+                                          'video_id': '352843156367900',
+                                          'call_to_action': {'type': 'LIKE_PAGE', 'value': {'page': '103122378948499'}}
+                                            },
 
 
+                              },
+    }
+    data = AdAccount(id).create_ad_creative(fields=fields, params=params)
+    print(data)
 
-# def create_campaigns():
-#     fields = [
-#         'name',
-#     ]
-#     params = {
-#         'name': 'My Python Campaign4',
-#         'objective': 'PAGE_LIKES',
-#         'status': 'PAUSED',
-#         'special_ad_categories': [],
-#     }
-#
-#     data = (AdAccount(id).create_campaign(fields=fields, params=params))
-#     print(data)
-#
-#
-# create_campaigns()
+# create_video()
 
 
+def create_carousel():
+  fields = [
+  ]
+  params = {
+    'message': 'Browse our latest products',
+    'published': '0',
+    'child_attachments': [{
+      'link': '<link>',
+      'name': 'Product 1',
+      'description': '$4.99',
+      'image_hash': '<imageHash>'
+    },
+      {
+        'link': '<link>',
+        'name': 'Product 2',
+        'description': '$4.99',
+        'image_hash': '<imageHash>'
+      },
+      {
+        'link': '<link>',
+        'name': 'Product 3',
+        'description': '$4.99',
+        'image_hash': '<imageHash>'
+      },
+      {
+        'link': '<link>',
+        'name': 'Product 4',
+        'description': '$4.99',
+        'image_hash': '<imageHash>'
+      },
+    ],
+    'caption': 'WWW.EXAMPLE.COM',
+    'link': 'http://www.example.com/products',
+  }
+
+  data = Page(id).get_posts(fields=fields, params=params)
+  print(data)
 
 
-# def create_ad_sets():
-#     fields = [
-#             'name',
-#         ]
-#     params = {
-#             'name': 'My Python AdSet',
-#             'lifetime_budget': '52500',
-#             'start_time': '2022-1-17T11:46:57-0800',
-#             'end_time': '2022-2-24T11:46:57-0800',
-#             'campaign_id': '6290943650816',
-#             'bid_amount': '1370',
-#             'billing_event': 'IMPRESSIONS',
-#             'optimization_goal': 'POST_ENGAGEMENT',
-#             'targeting': {
-#                 'age_min': 20,
-#                 'age_max': 24,
-#                 'behaviors': [{'id': 6002714895372, 'name': 'All travelers'}],
-#                 'genders': [1],
-#                 'geo_locations': {'countries': ['US'],
-#                                   'regions': [{'key': '4081'}],
-#                                   'cities': [{'key': '777934', 'radius': 10, 'distance_unit': 'mile'}]},
-#                 'interests': [{'id': '6003139266461', 'name': 'Movies'}],
-#                 'life_events': [{'id': 6002714398172, 'name': 'Newlywed (1 year)'}],
-#                 'facebook_positions': ['feed'],
-#                 'publisher_platforms': ['facebook', 'audience_network']},
-#             'status': 'PAUSED',
-#         }
-#
-#     data = (AdAccount(id).create_ad_set(fields=fields, params=params, ))
-#     print(data)
-
-
-# create_ad_sets()
-
-
-
-# def create_ads():
-#     fields = [
-#         'preview_shareable_link'
-#     ]
-#     params = {
-#         'name': 'My Python Ad5',
-#         'adset_id': '6290943651216',
-#         'creative': {'creative_id': '6291098407816'},
-#         'status': 'PAUSED',
-#     }
-#
-#     data = (AdAccount(id).create_ad(fields=fields, params=params,))
-#     print(data)
-#
-#6291340340816
-# create_ads()
-
-
-
-# def ad_creative():
-#     fields = [
-#         'name',
-#     ]
-
-#     params = {
-#         'name': 'Python Creative',
-#         'object_story_spec': {'page_id': '103122378948499',
-#                               'link_data': {
-#                                   'image_hash': '',
-#                                   'link': 'www.alperensamil.com',
-#                                   'message': 'python message',
-#                                   "description": 'Python description',
-#                               }},
-#     }
-#     data = (AdAccount(id).create_ad_creative(fields=fields, params=params, ))
-#     print(data)
-
-
-# ad_creative()
+# create_carousel()

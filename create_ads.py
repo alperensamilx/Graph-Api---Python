4aa332cc8a434ff1370f08f0ec8d1a4f863d1f02
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2022-02-05 17:14:56 +0300

    Update create_ads.py

--- create_ads.py
+++ create_ads.py
@@ -8,15 +8,46 @@ app_id = 'app-id'
 id = 'ad-account-id'
 FacebookAdsApi.init(access_token=access_token)
 
-fields = [
-]
-params = {
-  'name': 'My Python Ad',
-  'adset_id': 'ad-set-id',
-  'creative': {'creative_id': 'creative-id'},
-  'status': 'PAUSED',
-}
 
-print(AdAccount(id).create_ad(fields=fields,params=params,))
+def create_ads():
+    ad_set_id = str(input('Ad set ID: '))
+    name = str(input('Ad Name: '))
+    creative_id = str(input('Creative ID: '))
+    status = str(input('Status: '))
+    print("'since':YYYY-MM-DD,'until':YYYY-MM-DD'")
+    time_range = {'since': 2022-1-30, 'until': 2022-2-1}
+
+    fields = [
+        'name',
+        'preview_shareable_link'
+    ]
+    params = {
+        'name': name,
+        'adset_id': ad_set_id,
+        'creative': {'creative_id': creative_id},
+        'status': status,
+    }
+
+    data = AdAccount(id).create_ad(fields=fields, params=params,)
+    print(data)
 
 
+create_ads()
+
+
+# def create_ads():
+#     fields = [
+#         'preview_shareable_link'
+#     ]
+#     params = {
+#         'name': 'My Python Ad5',
+#         'adset_id': '6290943651216',
+#         'creative': {'creative_id': '6291098407816'},
+#         'status': 'PAUSED',
+#     }
+#
+#     data = (AdAccount(id).create_ad(fields=fields, params=params,))
+#     print(data)
+#
+#6291340340816
+# create_ads()
